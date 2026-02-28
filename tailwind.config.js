50a7fedb7788ece8b59ed50ed1a8c7bcbe33315f
Prince Radadiya <princeradadiya@example.com>
2026-02-28 08:26:05 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
